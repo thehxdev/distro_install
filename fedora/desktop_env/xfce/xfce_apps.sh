99ef341e00c3ef0a770ceed7cb9a9fe500f0204b
hxdevlover <xclimax1324@gmail.com>
2022-09-11 16:09:18 +0430

    updated

---
+++ fedora/desktop_env/xfce/xfce_apps.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+xargs sudo dnf install < ../../package_list/xfce/xfce_apps.txt -y
+
--- fedora/desktop_env/xfce/xfce_desktop.sh
+++ fedora/desktop_env/xfce/xfce_desktop.sh
@@ -2,6 +2,7 @@
 
 xargs sudo dnf install < ../../package_list/xfce/xfce_desktop.txt -y
 
+sudo systemctl disable xdm
 sudo systemctl enable lightdm
 sudo systemctl set-default graphical.target
 
