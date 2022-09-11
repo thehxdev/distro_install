e7cc7f3dff6b8ee11a848650e1a405ef14c5349c
hxdevlover <xclimax1324@gmail.com>
2022-09-11 21:59:23 +0430

    updated

--- fedora/desktop_env/xorg.sh
+++ fedora/desktop_env/xorg.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sudo dnf install @base-x -y
-sudo dnf install 'mesa*' 'libX*' 'fontconfig*' -y
+sudo dnf install 'mesa*' 'libX*' 'fontconfig*' 'xorg-x11-fonts-*' -y
 #sudo dnf install 'xorg-x11-*' 'mesa*' 'libX*' 'fontconfig*' -y
 
 sudo systemctl disable xdm
