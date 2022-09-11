99f5c676ee2c521f16c8d1eef41def36d8cf9408
hxdevlover <xclimax1324@gmail.com>
2022-09-11 17:12:49 +0430

    updated

--- fedora/desktop_env/xfce/xfce_apps.sh
+++ fedora/desktop_env/xfce/xfce_apps.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-xargs sudo dnf install < ../../package_list/xfce/xfce_apps.txt -y --setopt=install_weak_deps=False
+sudo echo "exclude='gnome*'" >> /etc/dnf/dnf.conf
+
+xargs sudo dnf install < ../../package_list/xfce/xfce_apps.txt -y
 
