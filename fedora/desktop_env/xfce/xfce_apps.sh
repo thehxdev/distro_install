e4bbb74469e6f76450e62b623f9834f5912cca0c
hxdevlover <xclimax1324@gmail.com>
2022-09-11 17:00:14 +0430

    updated

--- fedora/desktop_env/xfce/xfce_apps.sh
+++ fedora/desktop_env/xfce/xfce_apps.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-xargs sudo dnf install < ../../package_list/xfce/xfce_apps.txt -y
+xargs sudo dnf install < ../../package_list/xfce/xfce_apps.txt -y --setopt=install_weak_deps=False
 
