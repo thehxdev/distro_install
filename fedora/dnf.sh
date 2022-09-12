325af9f1b6aa5248ad4d72ba89e4c057c8f9b98e
hxdevlover <xclimax1324@gmail.com>
2022-09-13 00:06:16 +0430

    updated

--- fedora/dnf.sh
+++ fedora/dnf.sh
@@ -4,5 +4,8 @@ sudo echo "fastestmirror=True" >> /etc/dnf/dnf.conf
 
 #sudo dnf config-manager --add-repo < ./mirror.txt
 
+sudo mkdir /etc/repo_backup/
+sudo mv /etc/yum.repos.d/* /etc/repo_backup/
+
 sudo curl https://raw.githubusercontent.com/hxdevlover/distro_install/main/fedora/mirror_ir.repo >> /etc/yum.repos.d/iran_mirror.repo
 
