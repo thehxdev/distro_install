3f0ded3534b88ee071304d0f68da8ef97d8b94a7
hxdevlover <xclimax1324@gmail.com>
2022-09-14 21:31:35 +0430

    updated

--- fedora/base.sh
+++ fedora/base.sh
@@ -4,6 +4,6 @@ xargs sudo dnf install < ./package_list/base.txt -y
 
 sudo dnf groupinstall "Hardware Support" "Standard" "Fonts" "Input Methods" -y
 
-sudo dnf remove 'selinux*' -y
+sudo dnf remove selinux-policy selinux-policy-targeted -y
 
 #sudo dnf groupinstall "Printing Support"
