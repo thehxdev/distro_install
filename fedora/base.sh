f45ebe17e64f5ce449d6827a8bb189e0be3cbcfa
hxdevlover <xclimax1324@gmail.com>
2022-09-15 17:26:14 +0430

    updated

--- fedora/base.sh
+++ fedora/base.sh
@@ -2,7 +2,7 @@
 
 xargs sudo dnf install < ./package_list/base.txt -y
 
-sudo dnf groupinstall "Hardware Support" "Standard" "Fonts" "Input Methods" -y
+#sudo dnf groupinstall "Hardware Support" "Standard" "Fonts" "Input Methods" -y
 
 echo "Disabling selinux..." ; sleep 5 ; grubby --update-kernel ALL --args selinux=0
 
