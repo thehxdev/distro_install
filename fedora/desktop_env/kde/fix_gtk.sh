0dbff04abf22cb5504bdede61f102891cda61fa1
hxdevlover <xclimax1324@gmail.com>
2022-09-15 19:54:53 +0430

    updated

--- fedora/desktop_env/kde/fix_gtk.sh
+++ fedora/desktop_env/kde/fix_gtk.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-mkdir $HOME/.config/gtk-3.0/
-touch $HOME/.config/gtk-3.0/settings.ini
+#mkdir $HOME/.config/gtk-3.0/
 
 curl https://raw.githubusercontent.com/hxdevlover/distro_install/main/gtk/.gtkrc-2.0 >> $HOME/.gtkrc-2.0
+
 #curl https://raw.githubusercontent.com/hxdevlover/distro_install/main/gtk/settings.ini >> $HOME/.config/gtk-3.0/settings.ini
