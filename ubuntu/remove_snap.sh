337ce1a7e95683f0629042129af5b5df4bc810a7
hxdevlover <xclimax1324@gmail.com>
2022-09-22 12:31:56 +0330

    updated

--- ubuntu/remove_snap.sh
+++ ubuntu/remove_snap.sh
@@ -2,9 +2,9 @@
 
 #sudo snap list | cut -f 1 -d " " | xargs -L 1 sudo snap remove --purge
 
-sudo rm -rf /var/cache/snapd/
-
 sudo apt autoremove --purge snapd gnome-software-plugin-snap
+
+sudo rm -rf /var/cache/snapd/
 
 rm -fr ~/snap
 
