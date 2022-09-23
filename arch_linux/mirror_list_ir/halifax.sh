02560d7f2f9e2291d43162683da539b2a24d2e7e
hxdevlover <xclimax1324@gmail.com>
2022-09-23 18:45:06 +0330

    updated

--- arch_linux/mirror_list_ir/halifax.sh
+++ arch_linux/mirror_list_ir/halifax.sh
@@ -3,6 +3,6 @@
 sudo mkdir /etc/pacman.d/bak
 sudo mv /etc/pacman.d/mirrorlist /etc/pacman.d/bak
 sudo touch /etc/pacman.d/mirrorlist
-sudo echo "Server = https://ftp.halifax.rwth-aachen.de/archlinux/\$repo/os/\$arch" >> /etc/pacman.d/mirrorlist
+sudo echo "Server = http://ftp.halifax.rwth-aachen.de/archlinux/\$repo/os/\$arch" >> /etc/pacman.d/mirrorlist
 sudo pacman -Syy
 
