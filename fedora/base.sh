d980dfffd04b1b347503e78a81716dce262046fc
hxdevlover <xclimax1324@gmail.com>
2022-09-23 14:41:34 +0330

    updated

--- fedora/base.sh
+++ fedora/base.sh
@@ -2,7 +2,7 @@
 
 xargs sudo dnf install < ./package_list/base.txt -y
 
-echo "Disabling selinux..." ; sleep 5 ; grubby --update-kernel ALL --args selinux=0
+#echo "Disabling selinux..." ; sleep 5 ; grubby --update-kernel ALL --args selinux=0
 
 #sudo dnf groupinstall "Hardware Support" "Standard" "Fonts" "Input Methods" -y
 #sudo dnf groupinstall "Printing Support"
