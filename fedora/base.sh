700ddffa40d18334939672ab0862822f85a7c6eb
Hossein Khosravi <93550458+thehxdev@users.noreply.github.com>
2022-09-29 01:32:57 +0330

    Update base.sh

--- fedora/base.sh
+++ fedora/base.sh
@@ -5,4 +5,5 @@ xargs sudo dnf install < ./package_list/base.txt -y
 #echo "Disabling selinux..." ; sleep 5 ; grubby --update-kernel ALL --args selinux=0
 
 #sudo dnf groupinstall "Hardware Support" "Standard" "Fonts" "Input Methods" -y
+sudo dnf groupinstall "Fonts" -y
 #sudo dnf groupinstall "Printing Support"
