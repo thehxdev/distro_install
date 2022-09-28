832b2f9b6c455be4de399d39e5f0e0a2b83282ff
Hossein Khosravi <93550458+thehxdev@users.noreply.github.com>
2022-09-28 20:57:29 +0330

    Create firefox.sh

---
+++ ubuntu/firefox.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+sudo add-apt-repository ppa:mozillateam/ppa
+echo '
+Package: *
+Pin: release o=LP-PPA-mozillateam
+Pin-Priority: 1001
+' | sudo tee /etc/apt/preferences.d/mozilla-firefox
+
+sudo apt install firefox
